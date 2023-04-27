c553ed45d8da2924613e59455f680aefe0c0875b
starcks <soren.starck@etu.umontpellier.fr>
2023-04-27 19:48:18 +0200

    cover image problems

--- downloader.py
+++ downloader.py
@@ -1,30 +1,76 @@
 import os
-import pytube
+import urllib.request
+import youtube_dl
 from moviepy.editor import *
+from mutagen.id3 import ID3, APIC, error
+from mutagen.easyid3 import EasyID3
 
 # specify the playlist link
-playlist_link = "PUT_YOUR_PLAYLIST_LINK_HERE"
+playlist_link = "https://www.youtube.com/playlist?list=PLAteN_LndFJOikYpp4aABMhEJAY6A1_Oc"
 
-# create a PyTube playlist object
-playlist = pytube.Playlist(playlist_link)
+# set the options for youtube_dl
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(title)s.%(ext)s',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+}
 
-# create a folder to store the downloaded songs
-folder_name = playlist.title.replace(" ", "_").lower()
-os.makedirs(folder_name, exist_ok=True)
+# create a youtube_dl object
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    # get the playlist info
+    playlist_dict = ydl.extract_info(playlist_link, download=False)
 
-# iterate over each video in the playlist and download the audio in mp3 format
-for video in playlist.videos:
-    # get the audio stream of the video
-    audio_stream = video.streams.filter(only_audio=True, file_extension='mp4').first()
+    # create a folder to store the downloaded songs
+    folder_name = playlist_dict['title'].replace(" ", "_").lower()
+    os.makedirs(folder_name, exist_ok=True)
 
-    # download the audio stream
-    audio_stream.download(output_path=folder_name)
+    # iterate over each video in the playlist and download the audio in mp3 format
+    for video_dict in playlist_dict['entries']:
+        try:
+            # download the audio stream
+            ydl.download([video_dict['webpage_url']])
 
-    # convert the mp4 file to mp3
-    mp4_file = os.path.join(folder_name, audio_stream.default_filename)
-    mp3_file = os.path.join(folder_name, video.title.replace(" ", "_").lower() + ".mp3")
-    audio_clip = AudioFileClip(mp4_file)
-    audio_clip.write_audiofile(mp3_file)
+            # convert the downloaded file to mp3
+            mp4_file = f"{video_dict['title']}.mp3"
+            mp3_file = os.path.join(folder_name, video_dict['title'].replace(" ", "_").lower() + ".mp3")
+            audio_clip = AudioFileClip(mp4_file)
+            audio_clip.write_audiofile(mp3_file)
 
-    # delete the mp4 file
-    os.remove(mp4_file)
+            # delete the mp4 file
+            os.remove(mp4_file)
+
+            # add metadata to the mp3 file
+            audio = EasyID3(mp3_file)
+            audio['artist'] = video_dict.get('artist', playlist_dict.get('uploader', 'Unknown artist'))
+            audio['title'] = video_dict['title']
+
+            # download the cover image
+            thumbnail_url = video_dict.get('thumbnail', playlist_dict.get('thumbnail'))
+            if thumbnail_url:
+                try:
+                    with urllib.request.urlopen(thumbnail_url) as response:
+                        image_data = response.read()
+                        cover_image = image_data
+
+                        # add the cover image to the metadata
+                        audio['images'] = [APIC(
+                            encoding=3,
+                            mime='image/jpeg',
+                            type=3,
+                            desc=u'Cover',
+                            data=cover_image
+                        )]
+                except error:
+                    print(f"Could not add cover image to {mp3_file}.")
+                    print("error: ", error)
+
+            # save the metadata
+            audio.save()
+        except:
+            # if there is an error, skip the video and continue with the next one
+            print(f"Could not download {video_dict['title']}. Skipping...")
+            continue
